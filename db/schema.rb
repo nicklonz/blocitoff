ebf0c809ff9286447eba09e8d9cb539daa13e232
Nick L <nicklonz@gmail.com>
2014-06-29 21:42:31 -0400

    Added User to Lists

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140629140613) do
+ActiveRecord::Schema.define(version: 20140629141944) do
 
   create_table "items", force: true do |t|
     t.string   "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20140629140613) do
     t.string   "title"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
